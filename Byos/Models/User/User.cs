2c158f7481624b3163f76c575e594b1b29d1c408
Daniel Pana <daniel.pana@uipath.com>
2020-01-25 00:13:22 +0200

    Test10

--- Byos/Models/User/User.cs
+++ Byos/Models/User/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,15 @@ namespace Byos.Models
     public class User
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(50, ErrorMessage = "Name cannot exceed 50 characters")]
         public string Name { get; set; }
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", ErrorMessage = "Invalid Email Format")]
+        [Display(Name = "Office Email")]
         public string Email { get; set; }
+        [Required]
         public string Password { get; set; }
+        public Dept? Department { get; set; }
     }
 }
